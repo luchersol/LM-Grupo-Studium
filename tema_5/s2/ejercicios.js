5b3451438b597f4c6e9b7f25ca698643ea68b229
luchersol <lmherencia2003@gmail.com>
2026-04-29 01:56:45 +0200

    Add exercises and functionality for DOM manipulation in session 2

---
+++ tema_5/s2/ejercicios.js
@@ -0,0 +1,39 @@
+// #region EJERCICIO
+
+//   <input type="text" id="inputTexto" placeholder="Escribe algo">
+//   <button id="btnAgregar">Añadir</button>
+
+//   <ul id="lista"></ul>
+
+    const input = document.getElementById("inputTexto");
+    const boton = document.getElementById("btnAgregar");
+    const lista = document.getElementById("lista");
+
+    boton.addEventListener("click", function () {
+      const texto = input.value.trim();
+
+      // Evitar elementos vacíos
+      if (texto === "") return;
+
+      // Crear li
+      const li = document.createElement("li");
+      li.textContent = texto;
+
+      // Crear botón eliminar
+      const btnEliminar = document.createElement("button");
+      btnEliminar.textContent = "Eliminar";
+
+      // Evento eliminar
+      btnEliminar.addEventListener("click", () => li.remove());
+
+      // Añadir botón al li
+      li.appendChild(btnEliminar);
+
+      // Añadir li a la lista
+      lista.appendChild(li);
+
+      // Limpiar input
+      input.value = "";
+    });
+
+// #endregion
--- tema_5/s2/teoria.js
+++ tema_5/s2/teoria.js
@@ -2,10 +2,25 @@
 
 function agregarElemento() {
     const nuevoLi = document.createElement("li");
-    nuevoLi.textContent = "Nuevo elemento";
 
     const lista = document.getElementById("lista");
     lista.appendChild(nuevoLi);
+    // lista.append(nuevoLi);
+}
+
+function editarTexto() {
+    const texto = document.getElementById("texto-editable");
+
+    // OPCIÓN 1: REEMPLAZAR TODO EL CONTENIDO
+    texto.textContent = "Hola ";
+    
+    // OPCIÓN 2: AÑADIR SIN REEMPLAZAR
+    texto.append(document.createElement("div"));
+    // texto.textContent += " + añadido con textContent";
+
+    // OPCIÓN 3: UTILIZAR LOS NODOS
+    texto.append(" + añadido con append"); 
+    // texto.append(document.createTextNode("+ añadido con append"))
 }
 
 // #endregion
@@ -18,6 +33,12 @@ function eliminarElemento() {
     if (lista.children.length > 0) {
         lista.removeChild(lista.lastElementChild);
     }
+}
+
+function eliminarTexto() {
+    const texto = document.getElementById("texto-borrable");
+
+    texto.parentNode.removeChild(texto);
 }
 
 // #endregion
@@ -36,12 +57,26 @@ function verLocation() {
 
 function irAtras() {
     history.back();
+    // history.go(-1);
+}
+
+function idAdelante() {
+    history.forward();
+    // history.go(1);
+}
+
+function recargarPagina() {
+    location.reload();
 }
 
 function infoPantalla() {
-    alert("Ancho: " + screen.width + " Alto: " + screen.height);
+    alert(
+        "Ancho total: " + screen.width + "\n" +
+        "Alto total: " + screen.height + "\n" +
+        "Titulo: " + document.title + "\n" +
+        "Última modificación: " + document.lastModified
+    );
 }
-
 
 // #endregion
 
